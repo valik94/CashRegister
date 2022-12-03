63f1d39c790b3884830bf3f48c978bdffe010984
Valentin <valentin.lyash@gmail.com>
2022-12-03 15:38:37 -0500

    creating a product class to make a blueprint of the product object with 3 attributes, a constructor and 3 getters/setters to update the values of the attributes. The attributes include product type (pants, shirts, etc.), product price, and product quantity. Accordingly, the getters and setters will be updating these products

--- app/src/main/java/com/example/assignment_2/ListDataAdapter.java
+++ app/src/main/java/com/example/assignment_2/ListDataAdapter.java
@@ -1,4 +1,46 @@
 package com.example.assignment_2;
 
-public class ListDataAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class ListDataAdapter extends BaseAdapter {
+
+    //list of donations to render
+    //the context - getView function needs View and inflator to convert xml into layout object/view object
+    // this is done by creating a constructor that receives an arraylist of purchases and context
+
+    ArrayList<Product> list;
+    Context context;
+
+    //input(list of products, the view (i.e. MainActivity.this) )
+    public ListDataAdapter(ArrayList<Product> list, Context context){
+        this.list = list;
+        this.context = context;
+
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
 }
---
+++ app/src/main/java/com/example/assignment_2/Product.java
@@ -0,0 +1,4 @@
+package com.example.assignment_2;
+
+public class Product {
+}
