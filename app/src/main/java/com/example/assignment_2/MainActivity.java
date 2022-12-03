552a5b5b1ac96761c205875618ddde36844ab5e4
Valentin <valentin.lyash@gmail.com>
2022-12-03 15:31:19 -0500

    changing the architecture of the list row and ListDataAdapter to ensure we are able to display the selected/bought results in a list

--- app/src/main/java/com/example/assignment_2/MainActivity.java
+++ app/src/main/java/com/example/assignment_2/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.assignment_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    ListView purchaseList;
+
+    Button one, two, three, four, five, six, seven, eight, nine, zero, clear, buy, managerBtn;
+    TextView typeTV, quantityTV, totalTV;
+//    ListDataAdapter adapter;
+    Double total = 0.0;
+    ListView itemsListView;
+    int index = -1;
+    ArrayList<Product> stockList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView purchase_viewer = findViewById(R.id.purchase_list);
+        ListView purchase_viewer = findViewById(R.id.listview_products);
 
         String [] purchase_names = {"Pants", "Hats", "Shoes"};
         //Base Adapter instead
@@ -27,4 +46,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
 
